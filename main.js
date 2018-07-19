67ab9e13568d2c96681d4e261a1d9ffbfa9aaf40
italloleon <itallolaraujo@gmail.com>
2018-07-18 21:41:22 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     setTimeout(function () {
         $(".textoprincipal").fadeToggle("slow",function () {
-            $(".textoprincipal").text("Aguade para novidades");
+            $(".textoprincipal").text("Aguade para novidades...");
         }).delay( 800 ).fadeIn("slow");
     },3000)
 
